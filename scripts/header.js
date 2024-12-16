0631623f33df5ba0a9833ac3d0efe70f31701a36
itz_abdulfatai <134066346+itz-abdulfatai@users.noreply.github.com>
2024-12-16 20:27:03 +0100

    add quick fixes

--- scripts/header.js
+++ scripts/header.js
@@ -1,15 +1,26 @@
 const header = document.querySelector('header')
 const nav = document.querySelector('nav')
 
-document.addEventListener('scrollend', () => {
-    nav.classList.remove('active')
-    if (window.scrollY > 500) {
-        header.classList.add('passed')
-    } else {
-        
-        header.classList.remove('passed')
+let isScrolling = false;
+
+const scrollHandler = () => {
+    if (!isScrolling) {
+        window.requestAnimationFrame(() => {
+            nav.classList.remove('active');
+            if (window.scrollY > 500) {
+                header.classList.add('passed');
+            } else {
+                header.classList.remove('passed');
+            }
+            isScrolling = false;
+        });
+        isScrolling = true;
     }
-} )
+};
+
+document.addEventListener('scroll', scrollHandler);
+
+
 
 
 const navMenu = document.querySelector('.menu')
